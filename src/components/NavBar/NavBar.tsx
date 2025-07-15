b9dde15fad8b3fed75b0063a156bc0542090de8c
tomas eugui <mariotomaseugui@gmail.com>
2025-07-15 20:30:54 -0300

    componentes reutilizables navbar y btn

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,21 @@
+import {Link} from 'react-router-dom';
+
+import style from './NavBar.module.css';
+
+const Navbar = () =>{
+  return(
+    <nav className={style.navbar}>
+      <div className={style.container}>
+        <Link className={style.navbarBrand} to='/register'>Registrarse</Link>
+        <div className={style.navbarNav}>
+          <ul className={style.navbarNavList}>
+            <li className={style.navbarItem}><Link className={style.navbarLink} to='/posts'>Posts</Link></li>
+            <li className={style.navbarItem}><Link className={style.navbarLink}to='/users'>Usuarios</Link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
