824134564fac06e6d3f274086cccd5da6657b3af
tomas eugui <mariotomaseugui@gmail.com>
2025-07-15 08:45:57 -0300

    Configuracion rutas principales users y posts

---
+++ src/router/Router.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import RegisterPage from '../pages/Users/RegisterPage/RegisterPage';
+import UsersPage from '../pages/Users/Users/UsersPage';
+import PostsPage from '../pages/Posts/Posts/PostsPage';
+import CreatePostPage from '../pages/Posts/CreatePost/CreatePostPage';
+import EditPostPage from '../pages/Posts/EditPosts/EditPostPage';
+
+function Router(){
+    return(
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/users' element={<UsersPage />} />
+        <Route path='/posts' element={<PostsPage />} />
+        <Route path='/posts/createPost' element={<CreatePostPage />} />
+        <Route path='/posts/editPost/:id' element={<EditPostPage />} />
+        /**Agregar mas rutas si es necesario*/
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router;
